1032fbf00f4e494a99ae7caf50e9402f08224a0e
Nick Voss <nvoss@mail.usf.edu>
2016-03-10 19:24:25 -0500

    add cheese class

---
+++ cheese.py
@@ -0,0 +1,11 @@
+class Cheese(name,tast,cost,rheology):
+	def init(name,taste,cost,rheology):
+		self.name = name 
+		self.taste = taste
+		self.cost = cost
+		self.rheology = rheology
+	
+	def cut(self):
+		'''
+		cut the cheese			
+		'''
